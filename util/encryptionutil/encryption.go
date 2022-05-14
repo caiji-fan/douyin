8ed90508dd467a42f801a8668dc3c3f19bee3ce9
Fan <1163852344@qq.com>
2022-05-14 22:26:50 +0800

    增加了加密工具

---
+++ util/encryptionutil/encryption.go
@@ -0,0 +1,19 @@
+// Package encryptionutil
+// @Author shaofan
+// @Date 2022/5/14
+// @DESC 加密工具
+package encryptionutil
+
+// Encryption 			加密
+// src					需要加密的字符串
+// @return 				加密后的字符串
+func Encryption(src string) (string, error) {
+	return "", nil
+}
+
+// EncryptionCompare 	加密对比
+// src					原字符串
+// encryptionString 	待对比的加密字符串
+func EncryptionCompare(src string, encryptionString string) (bool, error) {
+	return false, nil
+}
