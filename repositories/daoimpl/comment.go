a79a82ed55d4244a277104fbe333ad073cf13f2e
Fan <1163852344@qq.com>
2022-05-15 16:58:59 +0800

    实现了评论dao层

--- repositories/daoimpl/comment.go
+++ repositories/daoimpl/comment.go
@@ -4,6 +4,7 @@
 package daoimpl
 
 import (
+	"douyin/entity/po"
 	"douyin/repositories"
 	"sync"
 )
@@ -11,7 +12,41 @@ import (
 type Comment struct {
 }
 
+func (c Comment) Insert(comment *po.Comment) error {
+	return db.Omit("ID", "create_time", "update_time").Create(comment).Error
+}
+
+func (c Comment) QueryByCondition(comment *po.Comment) (*[]po.Comment, error) {
+	var comments []po.Comment
+	if comment.ID != 0 {
+		return &comments, db.Where("id=?", comment.ID).Find(&comments).Error
+	}
+	db1 := db
+	if comment.SenderId != 0 {
+		db1 = db1.Where("sender_id = ?", comment.SenderId)
+	}
+	if comment.VideoId != 0 {
+		db1 = db1.Where("video_id = ?", comment.VideoId)
+	}
+	if comment.Status != 0 {
+		db1 = db1.Where("status = ?", comment.Status)
+	}
+	return &comments, db1.Model(comment).Find(&comments).Error
+}
+
+func (c Comment) UpdateByCondition(comment *po.Comment) error {
+	return db.Model(comment).Updates(comment).Error
+}
+
 var (
 	comment     repositories.Comment
 	commentOnce sync.Once
 )
+
+// NewCommentDaoInstance 获取评论dao实例
+func NewCommentDaoInstance() repositories.Comment {
+	commentOnce.Do(func() {
+		comment = Comment{}
+	})
+	return comment
+}
