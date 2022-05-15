77015870565cee4dc73cc5ed68861e2a0a645cca
Fan <1163852344@qq.com>
2022-05-15 10:28:57 +0800

    增加了数据库的初始化操作

--- server.go
+++ server.go
@@ -4,5 +4,17 @@
 // @DESC
 package main
 
+import (
+	"douyin/config"
+	"douyin/repositories"
+)
+
 func main() {
+}
+
+func init() {
+	// 配置文件初始化
+	config.Init()
+	// 数据库初始化
+	repositories.Init()
 }
