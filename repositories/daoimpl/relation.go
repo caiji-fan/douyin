5dcca3dfef9f2f74686cc8e22b5e856f23abaf8e
Fan <1163852344@qq.com>
2022-05-17 13:18:19 +0800

    初始化关注关系dao层

--- repositories/daoimpl/relation.go
+++ repositories/daoimpl/relation.go
@@ -3,5 +3,43 @@
 // @Date 2022/5/13
 package daoimpl
 
+import (
+	"douyin/entity/po"
+	"douyin/repositories"
+	"sync"
+)
+
 type Relation struct {
+}
+
+func (r Relation) Insert(follow *po.Follow) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r Relation) DeleteByCondition(follow *po.Follow) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r Relation) QueryFollowIdByFansId(fansId int) ([]int, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r Relation) QueryFansIdByFollowId(followId int) ([]int, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+var (
+	relation     repositories.Relation
+	relationOnce sync.Once
+)
+
+func NewRelationDaoInstance() repositories.Relation {
+	relationOnce.Do(func() {
+		relation = Relation{}
+	})
+	return relation
 }
