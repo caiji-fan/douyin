70138d266c04a60205b4edb1c1e48b5d18899bf8
Fan <1163852344@qq.com>
2022-05-18 20:13:10 +0800

    命名规范

--- middleware/auth.go
+++ middleware/auth.go
@@ -4,7 +4,10 @@
 package middleware
 
 import (
+	"douyin/config"
 	"douyin/util/jwtutil"
+	"douyin/util/redisutil"
+	"errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"strconv"
@@ -26,7 +29,6 @@ func JWTAuth(ctx *gin.Context) {
 		//获取参数userId
 		userId, err = getUserId(ctx)
 		if err != nil {
-			ctx.Abort()
 			return
 		}
 		//获取token
@@ -35,20 +37,17 @@ func JWTAuth(ctx *gin.Context) {
 	//解析token
 	err, uid := parseToken(ctx, tokenString)
 	if err != nil {
-		ctx.Abort()
 		return
 	}
 	//从redis判断token是否有效
 	err = tokenValid(ctx, userId)
 	if err != nil {
-		ctx.Abort()
 		return
 	}
 	//对比两个id是否一致 (投稿接口不需要此判断)
 	if !flag {
 		err = equalId(ctx, userId, uid)
 		if err != nil {
-			ctx.Abort()
 			return
 		}
 	} else {
@@ -67,6 +66,7 @@ func getUserId(ctx *gin.Context) (int, error) {
 			"status_code": http.StatusBadRequest,
 			"status_msg":  "未传入user_id",
 		})
+		ctx.Abort()
 	}
 	return userId, err
 }
@@ -81,19 +81,21 @@ func parseToken(ctx *gin.Context, token string) (error, int) {
 			"status_code": http.StatusBadRequest,
 			"status_msg":  "token已过期/未获取到token参数",
 		})
+		ctx.Abort()
 	}
 	return err, uid
 }
 
 //判断redis中token是否有效
 func tokenValid(ctx *gin.Context, userId int) error {
-	var redis_token string
-	err := redisutil.Get(config.Config.Redis.Key.Token+strconv.Itoa(userId), &redis_token)
-	if err != nil || redis_token == "" {
+	var redisToken string
+	err := redisutil.Get(config.Config.Redis.Key.Token+strconv.Itoa(userId), &redisToken)
+	if err != nil || redisToken == "" {
 		ctx.JSON(http.StatusBadRequest, gin.H{
 			"status_code": http.StatusBadRequest,
 			"status_msg":  "redis中token不存在",
 		})
+		ctx.Abort()
 		return errors.New("redis中token不存在")
 	}
 	return err
@@ -107,6 +109,7 @@ func equalId(ctx *gin.Context, userId int, uId int) error {
 			"status_code": http.StatusBadRequest,
 			"status_msg":  "无权限",
 		})
+		ctx.Abort()
 		return errors.New("id不一致")
 	}
 	return nil
