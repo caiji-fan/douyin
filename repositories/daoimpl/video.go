9e34d7da5156b4ad79c74e07f4ecbcec19a44c8f
wub <1814107106@qq.com>
2022-05-21 15:15:40 +0800

    video dao层

--- repositories/daoimpl/video.go
+++ repositories/daoimpl/video.go
@@ -6,8 +6,9 @@ package daoimpl
 import (
 	"douyin/entity/po"
 	"douyin/repositories"
-	"gorm.io/gorm"
 	"sync"
+
+	"gorm.io/gorm"
 )
 
 type Video struct {
@@ -23,43 +24,71 @@ func (v Video) QueryVideosByUserId(userId int) (*[]po.Video, error) {
 }
 
 func (v Video) QueryForUpdate(videoId int) (*po.Video, error) {
-	//TODO implement me
-	panic("implement me")
+	db1 := db
+	var video po.Video
+	err := db1.Raw("SELECT * FROM dy_video WHERE id = ? FOR UPDATE", videoId).Scan(&video).Error
+	return &video, err
 }
 
 func (v Video) Begin() (tx *gorm.DB) {
-	//TODO implement me
-	panic("implement me")
+	return db.Begin()
 }
 
 func (v Video) UpdateByCondition(video *po.Video, tx *gorm.DB, isTx bool) error {
-	//TODO implement me
-	panic("implement me")
+	var client *gorm.DB
+	if isTx {
+		client = tx
+	} else {
+		client = db
+	}
+	return client.Model(video).Updates(video).Error
 }
 
 func (v Video) QueryById(id int) (*po.Video, error) {
-	//TODO implement me
-	panic("implement me")
+	db1 := db
+	var video po.Video
+	if id != 0 {
+		db1 = db1.Where("id = ?", id)
+	}
+	err := db1.Find(&video).Error
+	return &video, err
 }
 
 func (v Video) Insert(video *po.Video) error {
-	//TODO implement me
-	panic("implement me")
+	return db.Omit("ID", "create_time", "update_time").Create(video).Error
 }
 
 func (v Video) QueryBatchIds(videoIds []int) (*[]po.Video, error) {
-	//TODO implement me
-	panic("implement me")
+	db1 := db
+	videoList := make([]po.Video, len(videoIds))
+	err := db1.Where("id IN ?", videoIds).Find(&videoList).Error
+	return &videoList, err
 }
 
 func (v Video) QueryByConditionTimeDESC(condition *po.Video) (*[]po.Video, error) {
-	//TODO implement me
-	panic("implement me")
+	db1 := db
+	var videos []po.Video
+	if condition.ID != 0 {
+		db1 = db1.Where("id = ?", condition.ID)
+	}
+	if condition.AuthorId != 0 {
+		db1 = db1.Where("author_id = ?", condition.AuthorId)
+	}
+	if condition.Title != "" {
+		db1 = db1.Where("name = ?", condition.Title)
+	}
+	err := db1.Order("update_time desc").Find(&videos).Error
+	return &videos, err
 }
 
 func (v Video) QueryByLatestTimeDESC(latestTime string) (*[]po.Video, error) {
-	//TODO implement me
-	panic("implement me")
+	db1 := db
+	var videos []po.Video
+	if latestTime != "" {
+		db1 = db1.Where("update_time < ?", latestTime)
+	}
+	err := db1.Order("update_time desc").Find(&videos).Error
+	return &videos, err
 }
 
 var (
