c51a12a2750be2b43966fd7b34aed0d05170805a
wub <1814107106@qq.com>
2022-05-21 15:46:59 +0800

    entity/response/video

--- entity/response/video.go
+++ entity/response/video.go
@@ -2,3 +2,17 @@
 // @Author shaofan
 // @Date 2022/5/19
 package response
+
+import "douyin/entity/bo"
+
+// 视频这一块的返回应该是啥？单个的视频？视频列表？
+
+type VideoList struct {
+	Response
+	Data []bo.Video `json:"video_list"`
+}
+
+type Video struct {
+	Response
+	Data bo.Video `json:"video"`
+}
