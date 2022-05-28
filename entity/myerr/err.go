1f30ac46f366567f302c47d96eb2e190c20449e6
Fan <1163852344@qq.com>
2022-05-28 14:10:11 +0800

    去除了redis设置过期时间失败的err

--- entity/myerr/err.go
+++ entity/myerr/err.go
@@ -6,13 +6,12 @@ package myerr
 import "errors"
 
 var (
-	VideoNotFound    = errors.New("视频不存在")
-	UserNameExist    = errors.New("用户名已存在")
-	UserNotFound     = errors.New("用户不存在")
-	InvalidToken     = errors.New("用户凭证无效")
-	NoPermission     = errors.New("无权限")
-	LoginError       = errors.New("用户名或密码错误")
-	RedisExpireError = errors.New("过期时间设置失败")
+	VideoNotFound = errors.New("视频不存在")
+	UserNameExist = errors.New("用户名已存在")
+	UserNotFound  = errors.New("用户不存在")
+	InvalidToken  = errors.New("用户凭证无效")
+	NoPermission  = errors.New("无权限")
+	LoginError    = errors.New("用户名或密码错误")
 )
 
 // ArgumentInvalid 参数无效
